22303e5842ac696d65bb4de38c2b278c36abc1db
ShuyuanChuang <shuyuan.chuang@relianz.tw>
2024-03-27 17:28:35 +0800

    fix: 修正 message api

--- routes/messageBoard.route.js
+++ routes/messageBoard.route.js
@@ -1,16 +1,12 @@
 const router = express.Router();
 import express from "express";
 
-import { getMessageBoards, getMessageBoard, createMessageBoard, updateMessageBoard, deleteMessageBoard } from "../controllers/messageBoard.controller.js";
+import { getMessageBoard, createMessageBoard, updateMessageBoard } from "../controllers/messageBoard.controller.js";
 
-router.get("/", getMessageBoards);
-
-router.get("/:id", getMessageBoard);
+router.get("/", getMessageBoard);
 
 router.post("/", createMessageBoard);
 
 router.put("/:id", updateMessageBoard)
-
-router.delete("/:id", deleteMessageBoard);
 
 export default router;
