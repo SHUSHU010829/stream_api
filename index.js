277ea97d50dc93da1bc3472249fbed66ee6891b9
SHUSHU010829 <shushu90829@gmail.com>
2024-05-05 07:02:39 +0800

    feat: delete websocket

--- index.js
+++ index.js
@@ -4,31 +4,11 @@ import colors from "colors";
 import createError from "http-errors";
 import dotenv from "dotenv";
 import cors from "cors";
-import WebSocket from "ws";
-import http from "http";
 dotenv.config();
 
 import songListRoutes from "./routes/songList.route.js";
 
 const app = express();
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
-// WebSocket 服务器逻辑
-wss.on("connection", function connection(ws) {
-  ws.on("message", function incoming(message) {
-    console.log("received: %s", message);
-  });
-
-  // 示例：定时发送消息
-  setInterval(() => {
-    const data = JSON.stringify({
-      type: "update",
-      message: "New data available",
-    });
-    ws.send(data);
-  }, 1000); // 每秒发送一次更新消息
-});
 
 // Middleware
 app.use((req, res, next) => {
